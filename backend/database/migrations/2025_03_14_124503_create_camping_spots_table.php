e2655dbcda3ca2d248668427adb9e3aafaf4a147
Roger Navarro <belegund@gmail.com>
2025-03-14 14:48:06 +0100

    feat(migrations): add Accomodation subtables

---
+++ backend/database/migrations/2025_03_14_124503_create_camping_spots_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('camping_spots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('accommodation_id')->constrained()->onDelete('cascade');
+            $table->integer('area_size_m2');
+            $table->boolean('has_electricity');
+            $table->boolean('accepts_caravan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('camping_spots');
+    }
+};
