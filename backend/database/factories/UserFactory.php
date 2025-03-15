fd5cc97e4758acca56b2d6de3d4f735888be0e23
Roger Navarro <belegund@gmail.com>
2025-03-15 18:31:13 +0100

    feat(back/seeders): Add User factory and seeder

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -12,11 +11,6 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -24,10 +18,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name_1' => fake()->lastName(),
+            'last_name_2' => fake()->optional()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'password',
+            'birthdate' => fake()->date('Y-m-d', '-18 years'),
+            'address' => fake()->address(),
+            'document_type' => fake()->randomElement(['DNI', 'NIE', 'Passport']),
+            'document_number' => Str::random(random_int(10, 20)),
+            'phone' => fake()->phoneNumber(),
+            'role' => fake()->randomElement(['user', 'admin']),
+            'comments' => fake()->text(100),
             'remember_token' => Str::random(10),
         ];
     }
@@ -35,10 +38,13 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
+    // Not in use for now, no email verification implemented
+    /*
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
+    */
 }
