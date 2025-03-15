e56c59f8e18715e0f2264a1349b1d3852a7460f6
Roger Navarro <belegund@gmail.com>
2025-03-15 13:03:13 +0100

    feat(models): add Accommodation and subtypes
    The subtypes are: Bungalow, CampingSpot, House, Room

--- backend/app/Models/Accommodation/Accommodation.php
+++ backend/app/Models/Accommodation/Accommodation.php
@@ -2,9 +2,52 @@
 
 namespace App\Models\Accommodation;
 
+use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Accommodation extends Model
+abstract class Accommodation extends Model
 {
-    //
+    protected $fillable = [
+        'accommodation_code',
+        'section',
+        'capacity',
+        'price_per_day',
+        'is_available',
+        'comments',
+        'type'
+    ];
+
+    protected $casts = [
+        'capacity' => 'integer',
+        'price_per_day' => 'integer',
+        'is_available' => 'boolean',
+        'type' => 'string' // Ensures the Enum `type` is always stored and retrieved as a string
+    ];
+
+    /**
+     * Get all reservations linked to this accommodation.
+     *
+     * One accommodation can have multiple reservations.
+     */
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class, 'accommodation_id');
+    }
+
+    /**
+     * Sets the `type` attribute automatically when creating an accommodation.
+     *
+     * The `type` field is an ENUM that stores the class name as a value.
+     * This makes creating accommodations slightly slower, but searching by type much faster.
+     */
+    protected static function boot()
+    {
+        // Calls Laravel's default boot() to keep important Eloquent features working.
+        parent::boot();
+
+        static::creating(function ($accommodation) {
+            $accommodation->type = class_basename($accommodation);
+        });
+    }
 }
--- backend/app/Models/Accommodation/House.php
+++ backend/app/Models/Accommodation/House.php
@@ -4,9 +4,31 @@ namespace App\Models\Accommodation;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class House extends Model
 {
-    /** @use HasFactory<\Database\Factories\Accommodation\HouseFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'bed_amount',
+        'room_amount',
+        'has_air_conditioning'
+    ];
+
+    protected $casts = [
+        'bed_amount' => 'integer',
+        'room_amount' => 'integer',
+        'has_air_conditioning' => 'boolean'
+    ];
+
+    /**
+     * Get the main accommodation details for this House.
+     *
+     * A House belongs to an accommodation.
+     */
+    public function accommodation(): BelongsTo
+    {
+        return $this->belongsTo(Accommodation::class, 'accommodation_id');
+    }
 }
