fa6c01ccd9b60ad0b51fbfd2a5b488e7225f729e
Roger Navarro <belegund@gmail.com>
2025-03-28 13:40:57 +0100

    feat(back/auth) implement basic authentication
    Only one endpoint implemented and tested just to make sure everything is in order (store Reservation)

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return response()->json([
+            'token' => $user->createToken('default')->plainTextToken
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,34 @@
 <?php
 
 use App\Http\Controllers\AccommodationController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('accommodations', AccommodationController::class);
-Route::apiResource('reservations', ReservationController::class);
+// USERS
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+// ACCOMMODATIONS
+Route::get('/accommodations', [AccommodationController::class, 'index']);
+Route::post('/accommodations', [AccommodationController::class, 'store']);
+Route::get('/accommodations/{accommodation}', [AccommodationController::class, 'show']);
+Route::put('/accommodations/{accommodation}', [AccommodationController::class, 'update']);
+Route::delete('/accommodations/{accommodation}', [AccommodationController::class, 'destroy']);
+
+// RESERVATIONS
+Route::get('/reservations', [ReservationController::class, 'index']);
+Route::middleware('auth:sanctum')->post('/reservations', [ReservationController::class, 'store']);
+Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
+Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
+Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
+
+
+Route::post('/login', [AuthController::class, 'login']);
