0b425225b5f652e71bb76dac1361de5736b819db
VenferD <vicente.n.fernandez@gmail.com>
2025-05-07 22:46:29 +0200

    style(header) Se aplica estilos al header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from 'app/services/auth.service';
@@ -8,7 +8,7 @@ import { AuthService } from 'app/services/auth.service';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   usuarioLogueado: any = null;
