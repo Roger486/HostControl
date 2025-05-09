75a82657d0a9bb84d829cf1a74a6e3864f0844f5
VenferD <vicente.n.fernandez@gmail.com>
2025-05-09 12:28:55 +0200

    style(forms):Se aplica estilos a confirmacion de registro. Se solucionan enlaces incompletos a home y register

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
@@ -7,7 +8,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
--- frontend/src/app/pages/registro-confirmado/registro-confirmado.component.ts
+++ frontend/src/app/pages/registro-confirmado/registro-confirmado.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro-confirmado',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './registro-confirmado.component.html',
   styleUrl: './registro-confirmado.component.css'
 })
